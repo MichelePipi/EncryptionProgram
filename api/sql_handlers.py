33853115b6cfaefe658c5d7ae42bdb649cb6ed8f
Michele <michele@michelepip.xyz>
2023-09-10 08:31:06 +0930

    PEP 8

--- api/sql_handlers.py
+++ api/sql_handlers.py
@@ -2,8 +2,10 @@ import sqlite3
 
 FILE_PATH = 'ciphers.sql'
 
+
 def create_connection() -> sqlite3.Connection:
     return sqlite3.connect(FILE_PATH, check_same_thread=False)
+
 
 def delete_table(connection: sqlite3.Connection):
     delete_table_query = "DROP TABLE IF EXISTS ciphers"
@@ -17,6 +19,7 @@ def create_table(connection: sqlite3.Connection):
                             ciphered VARCHAR(255)
                         )"""
     connection.execute(create_table_query)
+
 
 def commit_changes(connection: sqlite3.Connection):
     connection.commit()
--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def cipher_test(text=None, key=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
